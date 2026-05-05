7a41b2eedc3856b8cf3a6b858031992284f4ed25
thebarbalho <victorbarbalhomartins@gmail.com>
2026-05-05 15:15:27 -0300

    Update stellvertreter.py

--- stellvertreter.py
+++ stellvertreter.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-st.dataframe(df)
-
 # INTERFACE NO STREAMLIT
 st.title('DEPUTADOS FEDERAIS - 2022')
 st.image('https://www.brasildefato.com.br/wp-content/uploads/2024/10/image_processing20200201-29235-11htggk.jpg')
 df = pd.read_csv('deputados_2022.csv')
-
+st.dataframe(df)
 
 # PROCURA POR DEPUTADOS FEDERAIS
 st.title('SEARCH DE DEPUTADOS FEDERAIS - 2022')
